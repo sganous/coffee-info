71ca4e8ca1ba92c4121a7340f62d59b12cf735ce
Sergey Ganous <sergeyg@adventresources.com>
2021-02-12 00:01:57 -0800

    Add React Awesome Button
    * Added react-awesome-button library.
    * Added a AwesomeButtonProgress for displaying animated message.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  AwesomeButton,
+  AwesomeButtonProgress,
+  AwesomeButtonSocial,
+} from 'react-awesome-button';
+import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <button className="coffee-button">
-        Does Serge need coffee?
-      </button>
+      <AwesomeButtonProgress
+      type='primary'
+      size='large'
+      loadingLabel='Hmm...'
+      resultLabel='Obviously!'
+      action={(element, next) => {
+        setTimeout(() => {
+          next();
+        }, 1200);
+      }}
+      >
+      Do I need coffee?
+      </AwesomeButtonProgress>
       </header>
     </div>
   );
